88d4e331e8b41da017c8ca21e74ceb5a102d44ad
高橋　知咲 <takahashichisaki@wendyMacBook-Air.local>
2025-06-25 23:52:09 +0900

    :bug: fix: 設定の追加

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,9 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.platforms = ["web", "ios", "android", "native"];
+config.resolver.alias = {
+  "react-native$": "react-native-web",
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
