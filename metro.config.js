0e8c23c28950e8b7a75137f380997ba0599b18d6
kuuwannya <kuuuuu05062525@gmail.com>
2025-06-30 00:18:05 +0900

    :adhesive_bandage: update: 設定の変更

--- metro.config.js
+++ metro.config.js
@@ -3,15 +3,8 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// ソースファイル拡張子の設定
-config.resolver.sourceExts.push("css");
-
 // 本番環境でのみNativeWindを適用
-if (process.env.NODE_ENV === "production") {
-  module.exports = withNativeWind(config, {
-    input: "./global.css",
-    configPath: "./tailwind.config.js",
-  });
-} else {
-  module.exports = config;
-}
+module.exports = withNativeWind(config, {
+  input: "./global.css",
+  configPath: "./tailwind.config.js",
+});
